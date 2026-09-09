5894bc3abfc94955442b22c8cdb5102d6d1aa859
davidcastrogouveia-crypto <david.castro.gouveia@escola.pr.gov.br>
2026-09-09 17:40:24 -0300

    me atualizei

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+const botao = document.querySelector ("#botaomensagem");
+const mensagem=document.querySelector("#mensagemInterativa");
+botao.addEventlistener("click",mostrarMensagem);
+function mostrarMensagem(){
+    mensagem.textContent=
+    "voce acabou de criar uma interaçao  com JavaScript";
+
+}
